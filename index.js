607d2f895c73f2e1ad768699560bf35f1e746607
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-06 23:16:55 +0200

    index js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ function solveRect(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
     if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-            + l + ",  and b = " + b);
+        console.log("Rectangle dimensions should be greater than zero:  l = " + l + ",  and b = " + b);
     }
     else {
         console.log("The area of the rectangle is " + rect.area(l, b));
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,3 @@
-exports.perimeter = (x, y) => (2 * (x + y));
+export function perimeter(x, y) { return (2 * (x + y)); }
 
-exports.area = (x, y) => (x * y);
+export function area(x, y) { return (x * y); }
