31a3613a8eb3e01af2303d728f813226d1fe8782
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-24 00:21:38 +0200

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var rect = require('./rectangle');
 
+
 function solveRect(l, b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    console.log("Solving for rectangle with l = "
+        + l + " and b = " + b);
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = " + l + ",  and b = " + b);
-    }
-    else {
-        console.log("The area of the rectangle is " + rect.area(l, b));
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l, b));
-    }
-}
 
-solveRect(3, 6);
-solveRect(3, 5);
+//solveRect(3, 6);
+//solveRect(3, 5);
 solveRect(0, 5);
-solveRect(-3, 5);
+//solveRect(-3, 5);
+
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,15 @@
-export function perimeter(x, y) { return (2 * (x + y)); }
-
-export function area(x, y) { return (x * y); }
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() =>
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + x + ", and b = " + y),
+                null),
+            2000);
+    else
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            }),
+            2000);
+}
