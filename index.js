a5b214af543f27baa27e0e6666e8ad333610a6bf
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-24 13:10:45 +0200

    server

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-var rect = require('./rectangle');
+const http = require('http')
+
+const hostname = 'localhost'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><body><h1>Hello, World!</h1></body></html>')
+
+})
 
 
-function solveRect(l, b) {
-    console.log("Solving for rectangle with l = "
-        + l + " and b = " + b);
-    rect(l, b, (err, rectangle) => {
-        if (err) {
-            console.log("ERROR: ", err.message);
-        }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
 
-
-//solveRect(3, 6);
-//solveRect(3, 5);
-solveRect(0, 5);
-//solveRect(-3, 5);
 
