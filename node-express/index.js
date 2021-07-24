5fb49d81d3a142377925c9169aa0259df15878c4
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-24 23:14:50 +0200

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,18 +1,22 @@
 const express = require('express'),
     http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+//app.use((req, res, next) => {
+//    console.log(req.headers);
+//    res.statusCode = 200;
+//    res.setHeader('Content-Type', 'text/html');
+//    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+//
+//});
 
-});
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 
