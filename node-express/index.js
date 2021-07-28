54a7da0b2fa8449adadbe3e730ae34e54fb92b15
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-28 19:02:04 +0200

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
@@ -11,9 +11,6 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-//app.use(bodyParser.json());
-
-
 
 
 app.use('/dishes', dishRouter);
