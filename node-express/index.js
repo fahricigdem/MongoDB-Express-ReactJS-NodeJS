76d54f75f2cd046f489c694712285ac173521add
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-07-28 10:51:10 +0200

    promoRouter and leaderRouter added

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -15,6 +17,8 @@ app.use(morgan('dev'));
 
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
