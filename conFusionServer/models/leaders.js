ea6a33cc8a9f75a55757e4a575ff9236aeebf26b
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-02 23:51:32 +0200

    Assignment 2: MongoDB

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        equired: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leaders', leaderSchema);
+
+module.exports = Leaders;
