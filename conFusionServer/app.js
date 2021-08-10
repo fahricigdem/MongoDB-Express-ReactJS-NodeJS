94c6d3777fa7a9684fd1a6159e5bc8fd7ad3cb84
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-11 00:01:40 +0200

    Assignment 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favouriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -63,6 +64,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
