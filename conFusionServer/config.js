643bd4dea8c2259130b5252877ee0d52c510f33c
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-10 23:10:40 +0200

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '120439021965709',
+        clientSecret: '46209c6e76827f6e48cd84d4f5166f27'
+    }
 }
