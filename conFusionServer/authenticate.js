fd57991b4b6df2ca8a5b1d75448fcfa260c4b08d
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-11 13:33:53 +0200

    Backend and Frontend verknüpft

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,77 +1,103 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/user');
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var FacebookTokenStrategy = require('passport-facebook-token');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
-var config = require('./config.js');
+const config = require('./config');
 
-passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 exports.getToken = function (user) {
-    return jwt.sign(user, config.secretKey,
-        { expiresIn: 3600 });
+    return jwt.sign(
+        user,
+        config.secretKey,
+        {
+            expiresIn: 3600,
+        }
+    );
 };
 
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = config.secretKey;
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.secretKey,
+};
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload, done) => {
-        console.log("JWT payload: ", jwt_payload);
-        User.findOne({ _id: jwt_payload._id }, (err, user) => {
-            if (err) {
-                return done(err, false);
-            }
-            else if (user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
-            }
-        });
-    }));
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        opts,
+        (jwt_payload, done) => {
+            console.log("JWT payload: ", jwt_payload);
+            User.findOne(
+                { _id: jwt_payload._id },
+                (err, user) => {
+                    if (err) {
+                        return done(
+                            err,
+                            false
+                        );
+                    } else if (user) {
+                        return done(
+                            null,
+                            user
+                        );
+                    } else {
+                        return done(
+                            null,
+                            false
+                        );
+                    }
+                }
+            )
+        }
+    )
+);
 
 exports.verifyUser = passport.authenticate('jwt', { session: false });
 
+// verifyAdmin expects verifyUser to be called first
 exports.verifyAdmin = function (req, res, next) {
+    if (req.user.admin) {
+        return next();
+    }
 
     const err = new Error('You are not authorized to perform this operation!');
     err.status = 403;
-
-    !req.user.admin ? next(err) : next()
-
+    return next(err);
 };
 
-exports.facebookPassport = passport.use(new FacebookTokenStrategy({
-    clientID: config.facebook.clientId,
-    clientSecret: config.facebook.clientSecret
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ facebookId: profile.id }, (err, user) => {
-        if (err) {
-            return done(err, false);
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne(
+                { facebookId: profile.id },
+                (err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    if (!err && user !== null) {
+                        return done(null, user);
+                    }
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        }
+                        return done(null, user);
+                    });
+                }
+            );
         }
-        if (!err && user !== null) {
-            return done(null, user);
-        }
-        else {
-            user = new User({ username: profile.displayName });
-            user.facebookId = profile.id;
-            user.firstname = profile.name.givenName;
-            user.lastname = profile.name.familyName;
-            user.save((err, user) => {
-                if (err)
-                    return done(err, false);
-                else
-                    return done(null, user);
-            })
-        }
-    });
-}
-));
+    )
+);
